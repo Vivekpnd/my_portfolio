02bba24113e402609c44e5699fc3fc44a4786594
ssenterpiseswebsite1-spec <ssenterpiseswebsite1@gmail.com>
2026-03-08 22:14:44 +0530

    regenerate lock file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate"
+
 export default {
   content: [
     "./index.html",
@@ -7,5 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [animate],
 }
